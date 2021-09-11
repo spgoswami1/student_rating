3234528f216f50a7d102537ca9e0b510c3c4146c
Surendra <surendra.puri@newsbytesapp.com>
2021-09-11 13:57:58 +0530

    main logic added

--- predictor/views.py
+++ predictor/views.py
@@ -1,6 +1,7 @@
 import json
 from django.http import JsonResponse
 from rest_framework.views import APIView
+import pandas as pd
 
 
 class GenerateRating(APIView):
@@ -9,5 +10,13 @@ class GenerateRating(APIView):
 
     def get(self, request):
         username = request.GET["username"]
-        response = {"success": True, "message": "test", "data": username}
+        df = pd.read_csv('student_consistency.csv')
+        df = df[df['username']==username]
+        if df.shape[0] == 0:
+            response = {"success": False, "message": "Given Username Does not exist", "data": None}
+            return JsonResponse(response)
+
+        data = {"consistency_rating": df['consistency_rating'].values[0], "submission_rating": df['submission_rating'].values[0],
+        "overall_rating": df['final_rating'].values[0]}
+        response = {"success": True, "message": "test", "data": data}
         return JsonResponse(response)
